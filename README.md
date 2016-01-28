1ca5148ad87ec4708672809a64982c6ff3b2ed8e
Sreekanth <anubolusreekanth@gmail.com>
2016-01-29 01:57:59 +0530

    Current Location

---
+++ README.md
@@ -0,0 +1,2 @@
+# CurrentLocation
+A small app that tell you where you are right now, it gives you the co-ordinates of the current location and also gives you the address of the current co-ordinates, if Google has it :P.
---
+++ app/src/main/java/currentlocation/algotix/com/currentlocation/AddressActivity.java
@@ -0,0 +1,89 @@
+package currentlocation.algotix.com.currentlocation;
+
+import android.app.Activity;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class AddressActivity extends Activity {
+
+    Button btnShowLocation;
+
+    // GPSTracker class
+    GPSTracker gps;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
+
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new GPSTracker(AddressActivity.this);
+
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+                    String address = GetAddress(""+latitude, ""+longitude);
+                    // \n is for new line
+                    TextView locationView = (TextView) findViewById(R.id.location);
+                    String location= "Your location is: \nLat: " + latitude + "\nLong: " + longitude;
+                    locationView.setText(location);
+
+                    TextView addressView = (TextView) findViewById(R.id.address);
+                    addressView.setText(address);
+
+                    //Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
+    }
+
+    public String GetAddress(String lat, String lon)
+    {
+        Geocoder geocoder = new Geocoder(this, Locale.ENGLISH);
+        String ret = "";
+        try {
+            List<Address> addresses = geocoder.getFromLocation(Double.parseDouble(lat), Double.parseDouble(lon), 1);
+            if(addresses != null) {
+                Address returnedAddress = addresses.get(0);
+                StringBuilder strReturnedAddress = new StringBuilder("Address:\n");
+                for(int i=0; i<returnedAddress.getMaxAddressLineIndex(); i++) {
+                    strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
+                }
+                ret = strReturnedAddress.toString();
+            }
+            else{
+                ret = "No Address returned!";
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ret = "Can't get Address!";
+        }
+        return ret;
+    }
+
+}
